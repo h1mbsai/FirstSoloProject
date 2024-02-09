4bd45475c168c9c8438cfb7cec86955399165446
h1mbsai <159093087+h1mbsai@users.noreply.github.com>
2024-02-09 23:15:38 +0300

    Add files via upload

---
+++ myfirstsite/myfirstsite/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, View
+
+# Создание метода. В конце концов это то, что увидит пользователь. Это конечный файл после перехода по main/. здесь html шаблоны
+# к коду ниже - request(в переводе запрос) обязательный тег, необходим для работы метода.
+# <h4> - форматирование текста
+
+#def index(request): 
+#    return render(request, 'main/index.html' )
+
+class HomeView(TemplateView):
+    template_name = 'pages/index.html'
